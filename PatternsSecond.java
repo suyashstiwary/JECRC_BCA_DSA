de887d610cfc13e073165d61ad93f2bf2066a76f
suyashstiwary <suyash.studyhard@gmail.com>
2026-02-14 15:47:07 +0530

    Add files via upload

---
+++ PatternsSecond.java
@@ -0,0 +1,37 @@
+public class PatternsSecond {
+    public static void main(String[] args) {
+        int n=5;
+        // Q1
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++)System.out.print(" ");
+        //     for(int j=1;j<=2*i-1;j++)System.out.print("*");
+        //     for(int j=1;j<=n-i+1;j++)System.out.print(" ");
+        //     System.out.println("");
+            
+        // }
+
+        // Q2
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(i==1||i==n||j==1||j==n)System.out.print("*");
+        //         else System.out.print(" ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // Q3
+        // for(int i=1;i<=n;i++){
+        //     if(i>n/2){
+        //         for(int j=1;j<=n-i+1;j++){
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     else {
+        //         for(int j=1;j<=i;j++){
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println("");
+        // }
+    }
+}
